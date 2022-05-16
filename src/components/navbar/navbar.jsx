e9d987fc7078c2d7ea521a4185a87691dac9ce3c
dandanmania <danielkimus@gmail.com>
2022-05-16 15:15:54 -0700

    Add nearly empty director, genre, profile views and navbar

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+
+export function NavBar({user}) {
+    return (
+        <Navbar className="main-nav" sticky="top" expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand className="navbar-logo" href="/">myFlix</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responseive-navbar-nav">
+                    <Nav className="ml-auto">
+                        {isAuth() && (
+                            <Nav.Link href={`users/${user}`}>{user}</Nav.Link>
+                        )}
+                        {isAuth() && (
+                            <Button variant="link" onClick={() => { this.onLogOut() }}>Logout</Button>
+                        )}
+                        {!isAuth() && (
+                            <Nav.Link href="/">Sign-in</Nav.Link>
+                        )}
+                        {!isAuth() && (
+                            <Nav.Link href="/register">Sign-up</Nav.Link>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
