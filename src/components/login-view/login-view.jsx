e967a4a15e5dbbce7f260f82304c914022633a53
dandanmania <danielkimus@gmail.com>
2022-06-16 17:54:31 -0700

    Update Form Validation

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import {Button, Alert} from 'react-bootstrap';
 import axios from 'axios';
 
 import './login-view.scss'
@@ -10,9 +10,34 @@ import { Card } from 'react-bootstrap';
 export function LoginView(props) {
     const [ username, setUsername ] = useState('');
     const [ password, setPassword ] = useState('');
+    const [ usernameErr, setUsernameErr] = useState('');
+    const [ passwordErr, setPasswordErr ] = useState('');
+
+    //Validate User Inputs
+    const validate = () => {
+        let isReq = true;
+        if(!username){
+            setUsernameErr('Username Required');
+            isReq = false;
+        } else if(username.length < 2){
+            setUsernameErr('Username must be at least 2 characters long');
+            isReq = false.valueOf;
+        }
+        if(!password){
+            setPasswordErr('Password Required');
+            isReq = false;
+        } else if(password.length < 6){
+            setPassword('Password must be at least 6 characters long');
+            isReq = false;
+        }
+
+        return isReq;
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        const isReq = validate();
+        if(isReq) {
         axios.post('https://dandan-myflix.herokuapp.com/login', {
             Username: username,
             Password: password
@@ -23,7 +48,9 @@ export function LoginView(props) {
         })
         .catch(e => {
             console.log('No such user.')
+            alert('No such user.')
         })
+    }
     }
 
     return (
@@ -34,13 +61,15 @@ export function LoginView(props) {
                     <Form.Group className="mt-2" controlId="formUsername">
                         <Form.Label>Username:</Form.Label>
                         <Form.Control type="text" placeholder="Enter Username" onChange={e => setUsername(e.target.value)} />
+                        {usernameErr && <p>{usernameErr}</p>}
                     </Form.Group>
 
                     <Form.Group className="mt-2" controlId="formPassword">
                         <Form.Label>Password:</Form.Label>
                         <Form.Control type="password" placeholder="Enter Password" onChange={e => setPassword(e.target.value)} />
+                        {passwordErr && <p>{passwordErr}</p>}
                     </Form.Group>
-            
+
                     <Button className="mt-4" variant="secondary" type="submit" onClick={handleSubmit}>Log In</Button>
                     <Button className="mt-4" variant="link">Need to Register?</Button>
                 </Form>
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -12,9 +12,48 @@ export function RegistrationView(props) {
     const [ password, setPassword ] = useState('');
     const [ email, setEmail ] = useState('');
     const [ birthday, setBirthday ] = useState('');
+    const [ values, setValues] = useState({
+        usernameErr: '',
+        passwordErr: '',
+        emailErr: '',
+        birthdayErr: ''
+    })
+
+    const validate = () => {
+        let isReq = true;
+        if(!username){
+            setValues({...values, usernameErr: 'Username Required'});
+            isReq = false;
+        } else if(username.length < 2){
+            setValues({...values, usernameErr:'Username must be at least 2 characters long'});
+            isReq = false.valueOf;
+        }
+        if(!password){
+            setValues({...values, passwordErr:'Password Required'});
+            isReq = false;
+        } else if(password.length < 6){
+            setValues({...values, passwordErr:'Password must be at least 6 characters long'});
+            isReq = false;
+        }
+        if(!email){
+            setValues({...values, emailErr:'Email Required'});
+            isReq = false;
+        } else if(email.indexOf('@') === -1 ) {
+            setValues({...values, emailErr: 'Email is invalid'});
+            isReq = false;
+        }
+        if(!birthday){
+            setValues({...values, birthdayErr:'Birthday Required'});
+            isReq = false;
+        }
+
+        return isReq;
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        const isReq = validate();
+        if(isReq) {
         axios.post('https://dandan-myflix.herokuapp.com/users/', {
             Username: username,
             Password: password,
@@ -24,11 +63,14 @@ export function RegistrationView(props) {
         .then(response => {
             const data = response.data;
             console.log(data);
+            alert('Registration successful! Please Login!');
             window.open('/', '_self');
         })
         .catch(e => {
-            console.log('Error registering the user')
+            console.log('Error registering the user');
+            alert('Unable to register.');
         })
+    }
     }
 
     return (
@@ -39,21 +81,25 @@ export function RegistrationView(props) {
                     <Form.Group controlId="formUsername">
                         <Form.Label>Username:</Form.Label>
                         <Form.Control type="text" placeholder="Enter Username" onChange={e => setUsername(e.target.value)} />
+                        {values.usernameErr && <p>{values.usernameErr}</p>}
                      </Form.Group>
 
                     <Form.Group className="mt-2" controlId="formPassword">
                         <Form.Label>Password:</Form.Label>
                         <Form.Control type="password" placeholder="Enter Password" onChange={e => setPassword(e.target.value)} />
+                        {values.passwordErr && <p>{values.passwordErr}</p>}
                     </Form.Group>
             
                     <Form.Group className="mt-2" controlId="formEmail">
                         <Form.Label>Email:</Form.Label>
-                        <Form.Control type="email" placeholder="Enter Email" onChange={e => setEmail(e.target.value)} />    
+                        <Form.Control type="email" placeholder="Enter Email" onChange={e => setEmail(e.target.value)} />
+                        {values.emailErr && <p>{values.emailErr}</p>}    
                     </Form.Group>
 
                     <Form.Group className="mt-2" controlId="formBirthday">
                         <Form.Label>Birthday:</Form.Label>
-                        <Form.Control type="date" onChange={e => setBirthday(e.target.value)} />    
+                        <Form.Control type="date" onChange={e => setBirthday(e.target.value)} />
+                        {values.birthdayErr && <p>{values.birthdayErr}</p>}    
                     </Form.Group>
                     <Button className="mt-4" variant="secondary" type="submit" onClick={handleSubmit}>Submit</Button>
                     <Button className="mt-4" variant="link">Already Registered?</Button>
